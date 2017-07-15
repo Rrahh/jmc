1153cbfdbc6ab9874446ca52390d2d7058674a9a
Rrahh <chain@hitv.ru>
2017-07-15 17:38:12 +0300

    back
    Сделанное удаление не работает должным образом - вместо удаления требуемого промпта, удаляется следующий, если вызывать из триггера. Происходит из-за того, что во время вызова, первичная обработка уже прошла. Понял для чего был поставлен специальный символ для промпта - вернул.

--- sources/ttcoreex/telnet.cpp
+++ sources/ttcoreex/telnet.cpp
@@ -1029,15 +1029,8 @@ void do_telnet_protecol(const char* input, int length, int *used, char* output, 
 								if (!strPromptEndSeqBytes[PromptEndIndex]) { //match!
 									for (char *copy = (char*)strPromptEndReplBytes; *copy; )
 										output[(*generated)++] = *(copy++);
-									//output[(*generated)++] = END_OF_PROMPT_MARK;
+									output[(*generated)++] = END_OF_PROMPT_MARK;
 									PromptEndIndex = 0;
-									if (PromptDropCount){
-										PromptDropCount--;
-										while ((output[(*generated)-1] != '\n') && (--*generated));
-										output[*generated] = 0;//erase already written string
-										while ((input[(*used)] != '\n') && (++*used < length));//bypass rest of string
-										continue;
-									}
 								}
 								continue;
 							}
